54e268a5578e2b4bf5a4765fe3dfd1f251b1d84b
Stewart Lynch <slynch@createchsol.com>
2021-09-19 16:52:39 -0700

    Changed SignupView to a form

--- Sources/FBAuthentication/Views/SignUpView.swift
+++ Sources/FBAuthentication/Views/SignUpView.swift
@@ -15,51 +15,40 @@ struct SignUpView: View {
     @State private var showError = false
     @State private var errorString = ""
     var primaryColor: UIColor
-
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Group {
-                    VStack(alignment: .leading) {
-                        TextField("Full Name", text: self.$user.fullname).autocapitalization(.words)
-                        if !user.validNameText.isEmpty {
-                            Text(user.validNameText).font(.caption).foregroundColor(.red)
-                        }
-                    }
-                    VStack(alignment: .leading) {
-                        TextField("Email Address", text: self.$user.email).autocapitalization(.none).keyboardType(.emailAddress)
-                        if !user.validEmailAddressText.isEmpty {
-                            Text(user.validEmailAddressText).font(.caption).foregroundColor(.red)
-                        }
-                    }
-                    VStack(alignment: .leading) {
-                        SecureField("Password", text: self.$user.password)
-                        if !user.validPasswordText.isEmpty {
-                            Text(user.validPasswordText).font(.caption).foregroundColor(.red)
-                        }
-                    }
-                    VStack(alignment: .leading) {
-                        SecureField("Confirm Password", text: self.$user.confirmPassword)
-                        if !user.passwordsMatch(_confirmPW: user.confirmPassword) {
-                            Text(user.validConfirmPasswordText).font(.caption).foregroundColor(.red)
-                        }
-                    }
-                }.frame(width: 300)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                VStack(spacing: 20 ) {
+            Form {
+                TextField("Full Name", text: self.$user.fullname).autocapitalization(.words)
+                if !user.validNameText.isEmpty {
+                    Text(user.validNameText).font(.caption).foregroundColor(.red)
+                }
+                TextField("Email Address", text: self.$user.email).autocapitalization(.none).keyboardType(.emailAddress)
+                if !user.validEmailAddressText.isEmpty {
+                    Text(user.validEmailAddressText).font(.caption).foregroundColor(.red)
+                }
+                SecureField("Password", text: self.$user.password).autocapitalization(.none)
+                if !user.validPasswordText.isEmpty {
+                    Text(user.validPasswordText).font(.caption).foregroundColor(.red)
+                }
+                SecureField("Confirm Password", text: self.$user.confirmPassword).autocapitalization(.none)
+                if !user.passwordsMatch(_confirmPW: user.confirmPassword) {
+                    Text(user.validConfirmPasswordText).font(.caption).foregroundColor(.red)
+                }
+                HStack {
+                    Spacer()
                     Button(action: {
                         FBAuth.createUser(withEmail: self.user.email,
                                           name: self.user.fullname,
                                           password: self.user.password) { (restult) in
-                                            switch restult {
-                                            case .failure(let error):
-                                                self.errorString = error.localizedDescription
-                                                self.showError = true
-                                            case .success( _):
-                                                print("Account creation successful")
-                                            }
+                            switch restult {
+                            case .failure(let error):
+                                self.errorString = error.localizedDescription
+                                self.showError = true
+                            case .success( _):
+                                print("Account creation successful")
+                            }
                         }
-                        
                     }) {
                         Text("Register")
                             .frame(width: 200)
@@ -71,15 +60,17 @@ struct SignUpView: View {
                     }
                     .disabled(!user.isSignInComplete)
                     Spacer()
-                }.padding()
-            }.padding(.top)
-                .alert(isPresented: $showError) {
-                    Alert(title: Text("Error creating accout"), message: Text(self.errorString), dismissButton: .default(Text("OK")))
+                }
+                .padding(.top)
             }
-                .navigationBarTitle("Sign Up", displayMode: .inline)
-                .navigationBarItems(trailing: Button("Dismiss") {
-                    self.presentationMode.wrappedValue.dismiss()
-                })
+            .padding(.top)
+            .alert(isPresented: $showError) {
+                Alert(title: Text("Error creating accout"), message: Text(self.errorString), dismissButton: .default(Text("OK")))
+            }
+            .navigationBarTitle("Sign Up", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            })
         }
     }
 }
